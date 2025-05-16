c426afad34013d1fa194010b780a67cc0efd4669
dyno-jun <dyno.jun@gmail.com>
2025-05-16 14:32:15 +0900

    sonar test

---
+++ src/main/java/com/dyno/springserverstarter/SonaTest.java
@@ -0,0 +1,16 @@
+package com.dyno.springserverstarter;
+
+public class SonaTest {
+  public String test() {
+    return "";
+  }
+
+  public String test2() {
+    return test();
+  }
+
+
+  public String test3() {
+    return test3();
+  }
+}
