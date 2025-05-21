709a09c0c4d4cc801a2c9d54044bcd0a99d9de10
dyno-jun <dyno.jun@gmail.com>
2025-05-21 14:58:01 +0900

    git hook test

--- src/main/java/com/dyno/springserverstarter/SpringServerStarterApplication.java
+++ src/main/java/com/dyno/springserverstarter/SpringServerStarterApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringServerStarterApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(SpringServerStarterApplication.class, args);
   }
